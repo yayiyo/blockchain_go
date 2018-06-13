bf863473083ac9823d910fffd0f82de0e23a30e0
luckydog612 <luckydog612>
2018-06-13 16:10:14 +0800

    add validate pow

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	bc:=NewBlockchain()
+	bc := NewBlockchain()
 
 	bc.AddBlock("Send 1BTC to Ivan")
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	//遍历输出
-	for _,block:=range bc.blocks{
-		fmt.Printf("Prev.hash:%x\n",block.PrevBlockHash)
-		fmt.Printf("Data:%s\n",block.Data)
-		fmt.Printf("Hash:%x\n",block.Hash)
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev.hash:%x\n", block.PrevBlockHash)
+		fmt.Printf("Data:%s\n", block.Data)
+		fmt.Printf("Hash:%x\n", block.Hash)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW:%s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
