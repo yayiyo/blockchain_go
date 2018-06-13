3ff3037f2fc36a6fd47f51b90ccf1ac49b265f18
luckydog612 <luckydog612>
2018-06-13 16:09:21 +0800

    delete SetHash and update NewBlock

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"strconv"
-	"bytes"
-	"crypto/sha256"
 	"time"
 )
 
@@ -16,30 +13,22 @@ type Block struct {
 	//前一个区块的Hash值
 	PrevBlockHash []byte
 	//当前区块的Hash值
-	Hash []byte
-}
-
-//获取区块hash值
-func (b *Block) SetHash() {
-	//将时间戳转换成字节数组
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	//fmt.Println(timestamp)
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	//将区块信息进行hash
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
+	Hash  []byte
+	Nonce int
 }
 
 //创建区块
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
 
-	block.SetHash()
+	block.Hash = hash
+	block.Nonce = nonce
 	return block
 }
 
 //创建创世区块
 func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block",[]byte{})
+	return NewBlock("Genesis Block", []byte{})
 }
