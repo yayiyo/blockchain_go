3cb082cc73d98dfac5e1bc21ea130217e8cc1506
luckydog612 <luckydog612>
2018-06-14 11:13:48 +0800

    update NewBlock

--- block.go
+++ block.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"time"
+	"bytes"
+	"encoding/gob"
+	"log"
 )
 
 //定义一个区块
@@ -15,6 +18,19 @@ type Block struct {
 	//当前区块的Hash值
 	Hash  []byte
 	Nonce int
+}
+
+//序列化区块
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
 }
 
 //创建区块
@@ -31,4 +47,17 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 //创建创世区块
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+//反序列化
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder:=gob.NewDecoder(bytes.NewReader(d))
+	err:=decoder.Decode(&block)
+	if err!=nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
