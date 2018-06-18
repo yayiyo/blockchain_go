b0fbc94eba748777f9ff899bc417c75c9f933419
luckydog612 <luckydog612>
2018-06-18 13:11:05 +0800

    Implement ReverseBytes

--- utils.go
+++ utils.go
@@ -16,3 +16,10 @@ func IntToHex(num int64) []byte {
 
 	return buff.Bytes()
 }
+
+//将数组反转
+func ReverseBytes(data []byte) {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+}
