713a7269ba1a516fc45d3df6f37462abd2cd9fe2
luckydog612 <luckydog612>
2018-06-18 13:12:08 +0800

    Add wallets.go

---
+++ wallets.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"encoding/gob"
+	"crypto/elliptic"
+	"bytes"
+	"os"
+)
+
+//存储多个钱包的集合
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+//创建钱包集合，并在钱包集合存在的情况下读取出来
+func NewWallets() (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+
+	err := wallets.LoadFromFile()
+
+	return &wallets, err
+}
+
+//往钱包集合里添加一个新钱包
+func (ws *Wallets) CreateWallet() string {
+	wallet := NewWallet()
+	address := fmt.Sprintf("%s", wallet.GetAddress())
+
+	ws.Wallets[address] = wallet
+
+	return address
+}
+
+//获取钱包集合里的所有地址
+func (ws *Wallets) GetAddresses() []string {
+	var addresses []string
+	for address := range ws.Wallets {
+		addresses = append(addresses, address)
+	}
+
+	return addresses
+}
+
+//通过钱包地址获取钱包
+func (ws *Wallets) GetWallet(address string) Wallet {
+	return *ws.Wallets[address]
+}
+
+//从数据库中下载钱包集合
+func (ws *Wallets) LoadFromFile() error {
+	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
+		return err
+	}
+
+	fileContent, err := ioutil.ReadFile(walletFile)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var wallets Wallets
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&wallets)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ws.Wallets = wallets.Wallets
+
+	return nil
+}
+
+//将钱包集合保存到数据库
+func (ws Wallets) SaveToFile() {
+	var content bytes.Buffer
+
+	gob.Register(elliptic.P256())
+
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(ws)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
